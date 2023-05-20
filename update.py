8d103995d29fc0dc40d6c5dd37cfd99a79990eb6
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2023-05-20 22:53:22 +0200

    Convert tabs to spaces in update script

--- update.py
+++ update.py
@@ -20,34 +20,34 @@ source_paths = [
 ]
 
 boot2_variants = [
-	"generic_03h",
-	"at25sf128a",
-	"is25lp080",
-	"w25q080",
-	"w25x10cl"
+    "generic_03h",
+    "at25sf128a",
+    "is25lp080",
+    "w25q080",
+    "w25x10cl"
 ]
 
 def run(where, command, stdin=None):
-	print(command)
-	result = subprocess.run(command, shell=True, cwd=where, input=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	if result.returncode != 0:
-		print('failed exec command ' + command)
-		print(result.stderr.decode("utf-8").strip(" \n"))
-		exit(1)
-	return (result.returncode,
-	        result.stdout.decode("utf-8").strip(" \n"),
-	        result.stderr.decode("utf-8").strip(" \n"))
+    print(command)
+    result = subprocess.run(command, shell=True, cwd=where, input=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print('failed exec command ' + command)
+        print(result.stderr.decode("utf-8").strip(" \n"))
+        exit(1)
+    return (result.returncode,
+            result.stdout.decode("utf-8").strip(" \n"),
+            result.stderr.decode("utf-8").strip(" \n"))
 
 def bitrev(x, width):
     return int("{:0{w}b}".format(x, w=width)[::-1], 2)
 
 hw_def_re = re.compile(r'#define (.+)_hw \(\((.+)_hw_t \*const\)(.+)_BASE\)')
 def process(l):
-	hw_def = hw_def_re.match(l)
-	if hw_def:
-		print("replace hw def {}".format(hw_def[1]))
-		return "#define {}_hw (({}_hw_t*){}_BASE)".format(hw_def[1],hw_def[2],hw_def[3])
-	return l
+    hw_def = hw_def_re.match(l)
+    if hw_def:
+        print("replace hw def {}".format(hw_def[1]))
+        return "#define {}_hw (({}_hw_t*){}_BASE)".format(hw_def[1],hw_def[2],hw_def[3])
+    return l
 
 with urllib.request.urlopen("https://api.github.com/repos/raspberrypi/pico-sdk/releases/latest") as response:
    tag = json.loads(response.read())["tag_name"]
@@ -71,7 +71,7 @@ for pattern_conf in source_paths:
         if not path.is_file(): continue
         dest = path.relative_to(src_path)
         if len(pattern_conf) > 2:
-        	dest = Path(pattern_conf[2]) / dest
+            dest = Path(pattern_conf[2]) / dest
         dest.parent.mkdir(parents=True, exist_ok=True)
         print(dest)
         # Copy, normalize newline and remove trailing whitespace
@@ -82,42 +82,41 @@ for pattern_conf in source_paths:
 print("Building boot2 varints...")
 
 for variant in boot2_variants:
-	
-	builddir = Path('build/boot2_' + variant)
-	if not builddir.exists():
-		builddir.mkdir(parents=True, exist_ok=True)
-	run(builddir,"cmake ../../pico_sdk_src -G \"Unix Makefiles\" -DPICO_DEFAULT_BOOT_STAGE2=boot2_" + variant)
-	run(builddir,'make bs2_default_padded_checksummed_asm') # for validate
-	#run(builddir,'make bs2_default_bin')
-	ifile = os.path.join(builddir,'src/rp2_common/boot_stage2/bs2_default.bin')
-	try:
-	    idata = open(ifile, "rb").read()
-	except:
-		sys.exit("Could not open input file '{}'".format(ifile))
-	pad = 256
-	if len(idata) >= pad - 4:
-		sys.exit("Input file size ({} bytes) too large for final size ({} bytes)".format(len(idata), pad))
-	idata_padded = idata + bytes(pad - 4 - len(idata))
-	seed = 0xffffffff
-	# Our bootrom CRC32 is slightly bass-ackward but it's best to work around for now (FIXME)
-	# 100% worth it to save two Thumb instructions
-	checksum = bitrev(
-	    (binascii.crc32(bytes(bitrev(b, 8) for b in idata_padded), seed ^ 0xffffffff) ^ 0xffffffff) & 0xffffffff, 32)
-	odata = idata_padded + struct.pack("<L", checksum)
+    builddir = Path('build/boot2_' + variant)
+    if not builddir.exists():
+        builddir.mkdir(parents=True, exist_ok=True)
+    run(builddir,"cmake ../../pico_sdk_src -G \"Unix Makefiles\" -DPICO_DEFAULT_BOOT_STAGE2=boot2_" + variant)
+    run(builddir,'make bs2_default_padded_checksummed_asm') # for validate
+    #run(builddir,'make bs2_default_bin')
+    ifile = os.path.join(builddir,'src/rp2_common/boot_stage2/bs2_default.bin')
+    try:
+        idata = open(ifile, "rb").read()
+    except:
+        sys.exit("Could not open input file '{}'".format(ifile))
+    pad = 256
+    if len(idata) >= pad - 4:
+        sys.exit("Input file size ({} bytes) too large for final size ({} bytes)".format(len(idata), pad))
+    idata_padded = idata + bytes(pad - 4 - len(idata))
+    seed = 0xffffffff
+    # Our bootrom CRC32 is slightly bass-ackward but it's best to work around for now (FIXME)
+    # 100% worth it to save two Thumb instructions
+    checksum = bitrev(
+        (binascii.crc32(bytes(bitrev(b, 8) for b in idata_padded), seed ^ 0xffffffff) ^ 0xffffffff) & 0xffffffff, 32)
+    odata = idata_padded + struct.pack("<L", checksum)
 
-	ofilename = 'src/boot2_' + variant + '.cpp'
-	try:
-		Path(ofilename).parent.mkdir(parents=True, exist_ok=True)
-		with open(ofilename, "w") as ofile:
-			ofile.write("// Stage2 bootloader\n\n")
-			ofile.write("#include <cstdint>\n")
-			ofile.write("extern \"C\" __attribute__((section(\".boot2\"))) const uint8_t boot2[256] = {\n")
-			for offs in range(0, len(odata), 16):
-				chunk = odata[offs:min(offs + 16, len(odata))]
-				ofile.write("\t {},\n".format(", ".join("0x{:02x}".format(b) for b in chunk)))
-			ofile.write("};\n")
-	except:
-		sys.exit("Could not open output file '{}'".format(ofilename))
+    ofilename = 'src/boot2_' + variant + '.cpp'
+    try:
+        Path(ofilename).parent.mkdir(parents=True, exist_ok=True)
+        with open(ofilename, "w") as ofile:
+            ofile.write("// Stage2 bootloader\n\n")
+            ofile.write("#include <cstdint>\n")
+            ofile.write("extern \"C\" __attribute__((section(\".boot2\"))) const uint8_t boot2[256] = {\n")
+            for offs in range(0, len(odata), 16):
+                chunk = odata[offs:min(offs + 16, len(odata))]
+                ofile.write("\t {},\n".format(", ".join("0x{:02x}".format(b) for b in chunk)))
+            ofile.write("};\n")
+    except:
+        sys.exit("Could not open output file '{}'".format(ofilename))
 
 subprocess.run("git add src include LICENSE.TXT", shell=True)
 if subprocess.call("git diff-index --quiet HEAD --", shell=True):
